9281d0e514fd37cc8232bbe50f14654a21490dda
Charles Luz dos Santos <charlesluz.94@gmail.com>
2025-07-23 08:52:57 -0300

    remove unused import.

--- src/main/java/com/cluz/hex_client/adapters/in/controller/exceptions/Error.java
+++ src/main/java/com/cluz/hex_client/adapters/in/controller/exceptions/Error.java
@@ -3,9 +3,7 @@ package com.cluz.hex_client.adapters.in.controller.exceptions;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
-import java.time.LocalDateTime;
 import java.time.ZonedDateTime;
-import java.util.Date;
 
 @Data
 @AllArgsConstructor
